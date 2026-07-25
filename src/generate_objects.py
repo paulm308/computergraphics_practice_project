856c18fe99deeb43217d5e28eb4325b411e2f9c3
paulm <pm308@students.uni-freiburg.de>
2026-07-25 18:49:11 +0200

    Added light_computation and projection

--- src/generate_objects.py
+++ src/generate_objects.py
@@ -1,10 +1,6 @@
 import numpy as np
 
 
-verteces = []
-faces = []
-
-
 def generate_cube():
     cube_verteces = []
     for i in range(8, 16):
@@ -23,7 +19,19 @@ def generate_cube():
                   (3, 6, 7),
                   (4, 5, 6),
                   (5, 6, 7)]
-    return cube_verteces, cube_faces
+    cube_surface_normals = [(0., -1., 0.),
+                            (0., -1., 0.),
+                            (-1., 0., 0.),
+                            (-1., 0., 0.),
+                            (0., 0., -1.),
+                            (0., 0., -1.),
+                            (0., 0., 1.),
+                            (0., 0., 1.),
+                            (0., 1., 0.),
+                            (0., 1., 0.),
+                            (1., 0., 0.),
+                            (1., 0., 0.)]
+    return cube_verteces, cube_faces, cube_surface_normals
 
 
 def convert_to_homegeneous(verteces: list[tuple[float, float, float]]):
@@ -80,14 +88,40 @@ def shear(vector: tuple):
                      [0., 0., 0., 1.]])
 
 
+def normal_matrix(M):
+    return np.linalg.inv(M).T
+
+
+def normalize(vector):
+    length = np.linalg.norm(vector[:3])
+    result = vector.copy()
+    result[:3] = vector[:3] / length
+    return result
+
+
+def clean(array, tol=1e-10):
+    array = np.where(np.abs(array) < tol, 0.0, array)
+    return array
+
+
 if __name__ == "__main__":
-    cube_verteces, cube_faces = generate_cube()
+    cube_verteces, cube_faces, cube_surface_normals = generate_cube()
+
     cube_verteces = convert_to_homegeneous(cube_verteces)
     cube_verteces = convert_to_numpy(cube_verteces)
+    cube_surface_normals = convert_to_homegeneous(cube_surface_normals)
+    cube_surface_normals = convert_to_numpy(cube_surface_normals)
+
     t1 = translate((-0.5, -0.5, -0.5))
     r = rotate((90., 90., 0.))
     s = scale_and_mirror((2., 2., 2.))
     t2 = translate((1.5, 1.5, 1.5))
     m = t2 @ s @ r @ t1
-    cube_verteces = [m @ vertex.T for vertex in cube_verteces]
-    print(cube_verteces)
+
+    nr = normal_matrix(r)
+    ns = normal_matrix(s)
+    nm = ns @ nr
+
+    cube_verteces = [clean(m @ vertex.T) for vertex in cube_verteces]
+    cube_surface_normals = [clean(normalize(nm @ surface_normal.T)) for surface_normal in cube_surface_normals]
+    print(f"{cube_verteces}\n{cube_surface_normals}")
---
+++ src/project.py
@@ -0,0 +1,109 @@
+import numpy as np
+from generate_objects import normalize
+
+
+def compute_rays(num_x: int, num_y: int, focal_point_x: float):
+    pixel_width = 2 / max(num_x, num_y)
+    sensor_pixel_top_left_x = -(num_x * pixel_width) / 2 + 0.5 * pixel_width
+    sensor_pixel_top_left_y = -(num_y * pixel_width) / 2 + 0.5 * pixel_width
+
+    rays = []
+    for x in range(num_x):
+        for y in range(num_y):
+            sensor_pixel_x_pos = sensor_pixel_top_left_x + x * pixel_width
+            sensor_pixel_y_pos = sensor_pixel_top_left_y + y * pixel_width
+
+            sensor_pixel_pos = np.array([0., sensor_pixel_y_pos, sensor_pixel_x_pos, 1.])
+            focal_point = np.array([focal_point_x, 0., 0., 1.])
+            direction = normalize(sensor_pixel_pos - focal_point)
+            rays.append((sensor_pixel_pos, direction))
+    return rays
+
+
+def ray_triangle_intersect(origin, direction, verteces, epsilon=1e-8):
+    edge1 = verteces[1] - verteces[0]
+    edge2 = verteces[2] - verteces[0]
+
+    h = np.cross(direction, edge2)
+    a = np.dot(edge1, h)
+
+    if abs(a) < epsilon:
+        return None
+
+    f = 1.0 / a
+    s = origin - verteces[0]
+    u = f * np.dot(s, h)
+
+    if u < 0.0 or u > 1.0:
+        return None
+
+    q = np.cross(s, edge1)
+    v = f * np.dot(direction, q)
+
+    if v < 0.0 or u + v > 1.0:
+        return None
+
+    t = f * np.dot(edge2, q)
+
+    if t > epsilon:
+        return t
+    else:
+        return None
+
+
+def compute_intersections(rays: list, verteces: list, faces: list):
+    res = []
+    for ray in rays:
+        intersections = []
+        for face_index, face in enumerate(faces):
+            origin, direction = ray
+            face_verteces = [verteces[face[i]] for i in range(len(face))]
+            t = ray_triangle_intersect(origin, direction, face_verteces)
+            intersections += [(t, face_index)] if t is not None else []
+        if len(intersections) != 0:
+            intersection = min(intersections, key=lambda x: x[0])
+            res += [intersection]
+        else:
+            res += [None]
+    return res
+
+
+def compute_light(direction, light_positions, point, verteces, normal, faces, face_index, surface_atributes, light_atributes):
+    rho = surface_atributes[face_index][0]
+    rho_white = np.array([1., 1., 1.])
+    L_indirect = np.array([0.1, 0.1, 0.1])
+    alpha = surface_atributes[face_index][1]
+    beta = surface_atributes[face_index][2]
+    gamma = surface_atributes[face_index][3]
+    m = surface_atributes[face_index][4]
+    v = -direction
+    n = normal
+
+    light_sum = np.array([0., 0., 0.])
+    for light_index, light_pos in enumerate(light_positions):
+        blocked = False
+        raw_l_i = light_pos - point
+        l_i = normalize(raw_l_i)
+        for face in faces:
+            face_verteces = [verteces[face[i]] for i in range(len(face))]
+            t = ray_triangle_intersect(point, raw_l_i, face_verteces, epsilon=1e-8)
+            if t is not None and t < 1.0:
+                blocked = True
+                break
+        if blocked:
+            continue
+
+        L_i = light_atributes[light_index]
+        r_i = 2 * np.dot(n, l_i) * n - l_i
+
+        n_dot_l = max(np.dot(n, l_i), 0)
+        r_dot_v = max(np.dot(r_i, v), 0)
+
+        tmp = beta * rho + gamma * rho_white * (r_dot_v ** m)
+        light_sum += L_i * n_dot_l * tmp
+
+    return alpha * rho * L_indirect + light_sum
+
+
+def compute_lighting(faces: list):
+    pass
